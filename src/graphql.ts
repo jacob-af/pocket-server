fb099287cfd5d6393ab4c41f91fc88acc2f9ba23
jacob-af <jfeitler@gmail.com>
2024-04-02 10:50:45 -0400

    removed one return to redeply

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,4 +1,3 @@
-
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -9,134 +8,164 @@
 /* eslint-disable */
 
 export enum Relationship {
-    Favorite = "Favorite",
-    Close = "Close",
-    Following = "Following",
-    Blocked = "Blocked"
+  Favorite = 'Favorite',
+  Close = 'Close',
+  Following = 'Following',
+  Blocked = 'Blocked',
 }
 
 export class CreateUserInput {
-    userName: string;
-    email: EmailAddress;
-    password: string;
+  userName: string;
+  email: EmailAddress;
+  password: string;
 }
 
 export class LoginInput {
-    email: EmailAddress;
-    password: string;
+  email: EmailAddress;
+  password: string;
 }
 
 export class CreateIngredientInput {
-    name: string;
-    description?: Nullable<string>;
+  name: string;
+  description?: Nullable<string>;
 }
 
 export class UpdateIngredientInput {
-    id: string;
-    name: string;
-    description?: Nullable<string>;
+  id: string;
+  name: string;
+  description?: Nullable<string>;
 }
 
 export class UpdateUserInput {
-    id: string;
-    userName?: Nullable<string>;
-    email?: Nullable<string>;
+  id: string;
+  userName?: Nullable<string>;
+  email?: Nullable<string>;
 }
 
 export class AuthPayload {
-    accessToken: string;
-    refreshToken: string;
-    user: User;
+  accessToken: string;
+  refreshToken: string;
+  user: User;
 }
 
 export class LogoutResponse {
-    loggedOut: boolean;
+  loggedOut: boolean;
 }
 
 export class NewTokenResponse {
-    accessToken: string;
-    refreshToken: string;
+  accessToken: string;
+  refreshToken: string;
 }
 
 export abstract class IQuery {
-    abstract hello(): string | Promise<string>;
+  abstract hello(): string | Promise<string>;
 
-    abstract ingredients(): Nullable<Ingredient>[] | Promise<Nullable<Ingredient>[]>;
+  abstract ingredients():
+    | Nullable<Ingredient>[]
+    | Promise<Nullable<Ingredient>[]>;
 
-    abstract ingredient(id: number): Nullable<Ingredient> | Promise<Nullable<Ingredient>>;
+  abstract ingredient(
+    id: number,
+  ): Nullable<Ingredient> | Promise<Nullable<Ingredient>>;
 
-    abstract allUsers(): Nullable<User>[] | Promise<Nullable<User>[]>;
+  abstract allUsers(): Nullable<User>[] | Promise<Nullable<User>[]>;
 
-    abstract userById(id: string): Nullable<User> | Promise<Nullable<User>>;
+  abstract userById(id: string): Nullable<User> | Promise<Nullable<User>>;
 }
 
 export abstract class IMutation {
-    abstract login(loginInput: LoginInput): Nullable<AuthPayload> | Promise<Nullable<AuthPayload>>;
+  abstract login(
+    loginInput: LoginInput,
+  ): Nullable<AuthPayload> | Promise<Nullable<AuthPayload>>;
 
-    abstract signup(createUserInput: CreateUserInput): Nullable<AuthPayload> | Promise<Nullable<AuthPayload>>;
+  abstract signup(
+    createUserInput: CreateUserInput,
+  ): Nullable<AuthPayload> | Promise<Nullable<AuthPayload>>;
 
-    abstract logout(userId: string): Nullable<LogoutResponse> | Promise<Nullable<LogoutResponse>>;
+  abstract logout(
+    userId: string,
+  ): Nullable<LogoutResponse> | Promise<Nullable<LogoutResponse>>;
 
-    abstract getNewTokens(userId?: Nullable<string>, refreshToken?: Nullable<string>): Nullable<NewTokenResponse> | Promise<Nullable<NewTokenResponse>>;
+  abstract getNewTokens(
+    userId?: Nullable<string>,
+    refreshToken?: Nullable<string>,
+  ): Nullable<NewTokenResponse> | Promise<Nullable<NewTokenResponse>>;
 
-    abstract createIngredient(createIngredientInput: CreateIngredientInput): Ingredient | Promise<Ingredient>;
+  abstract createIngredient(
+    createIngredientInput: CreateIngredientInput,
+  ): Ingredient | Promise<Ingredient>;
 
-    abstract createManyIngredient(createIngredientInputs: Nullable<CreateIngredientInput>[]): StatusMessage | Promise<StatusMessage>;
+  abstract createManyIngredient(
+    createIngredientInputs: Nullable<CreateIngredientInput>[],
+  ): StatusMessage | Promise<StatusMessage>;
 
-    abstract updateIngredient(updateIngredientInput: UpdateIngredientInput): Ingredient | Promise<Ingredient>;
+  abstract updateIngredient(
+    updateIngredientInput: UpdateIngredientInput,
+  ): Ingredient | Promise<Ingredient>;
 
-    abstract removeIngredient(id: string): Nullable<Ingredient> | Promise<Nullable<Ingredient>>;
+  abstract removeIngredient(
+    id: string,
+  ): Nullable<Ingredient> | Promise<Nullable<Ingredient>>;
 
-    abstract followUser(followId: string, relationship?: Nullable<Relationship>): Nullable<StatusMessage> | Promise<Nullable<StatusMessage>>;
+  abstract followUser(
+    followId: string,
+    relationship?: Nullable<Relationship>,
+  ): Nullable<StatusMessage> | Promise<Nullable<StatusMessage>>;
 
-    abstract unFollowUser(unfollowId: string): Nullable<StatusMessage> | Promise<Nullable<StatusMessage>>;
+  abstract unFollowUser(
+    unfollowId: string,
+  ): Nullable<StatusMessage> | Promise<Nullable<StatusMessage>>;
 
-    abstract blockUser(blockId: string): Nullable<StatusMessage> | Promise<Nullable<StatusMessage>>;
+  abstract blockUser(
+    blockId: string,
+  ): Nullable<StatusMessage> | Promise<Nullable<StatusMessage>>;
 
-    abstract unblockUser(unblockId: string): Nullable<StatusMessage> | Promise<Nullable<StatusMessage>>;
+  abstract unblockUser(
+    unblockId: string,
+  ): Nullable<StatusMessage> | Promise<Nullable<StatusMessage>>;
 }
 
 export class Ingredient {
-    id: string;
-    name: string;
-    description?: Nullable<string>;
+  id: string;
+  name: string;
+  description?: Nullable<string>;
 }
 
 export class User {
-    id: string;
-    userName: string;
-    email: EmailAddress;
-    dateJoined?: Nullable<DateTime>;
-    lastEdited?: Nullable<DateTime>;
-    following?: Nullable<Nullable<Following>[]>;
-    followedBy?: Nullable<Nullable<Follower>[]>;
+  id: string;
+  userName: string;
+  email: EmailAddress;
+  dateJoined?: Nullable<DateTime>;
+  lastEdited?: Nullable<DateTime>;
+  following?: Nullable<Nullable<Following>[]>;
+  followedBy?: Nullable<Nullable<Follower>[]>;
 }
 
 export class Following {
-    id: string;
-    userName: string;
-    email: EmailAddress;
-    dateJoined?: Nullable<DateTime>;
-    lastEdited?: Nullable<DateTime>;
-    relationship?: Nullable<Relationship>;
+  id: string;
+  userName: string;
+  email: EmailAddress;
+  dateJoined?: Nullable<DateTime>;
+  lastEdited?: Nullable<DateTime>;
+  relationship?: Nullable<Relationship>;
 }
 
 export class Follower {
-    id: string;
-    userName: string;
-    email: EmailAddress;
-    dateJoined?: Nullable<DateTime>;
-    lastEdited?: Nullable<DateTime>;
+  id: string;
+  userName: string;
+  email: EmailAddress;
+  dateJoined?: Nullable<DateTime>;
+  lastEdited?: Nullable<DateTime>;
 }
 
 export class StatusMessage {
-    message?: Nullable<string>;
+  message?: Nullable<string>;
 }
 
 export class FollowReturn {
-    following?: Nullable<string>;
-    relationship?: Nullable<Relationship>;
-    status?: Nullable<StatusMessage>;
+  following?: Nullable<string>;
+  relationship?: Nullable<Relationship>;
+  status?: Nullable<StatusMessage>;
 }
 
 export type DateTime = any;
