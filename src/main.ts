51b727cc43ed2bc85796ae36bf62db8d19ff5228
jacob-af <jfeitler@gmail.com>
2024-05-18 22:50:39 -0400

    making sure prisma connections close

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import type { NestExpressApplication } from '@nestjs/platform-express';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.enableShutdownHooks();
   app.useBodyParser('json', { limit: '10mb' });
   await app.listen(process.env.PORT || 4000);
 }
