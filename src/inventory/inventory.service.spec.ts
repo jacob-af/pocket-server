805c939c01aa1ea587c177035da511a44152477c
jacob-af <jfeitler@gmail.com>
2024-06-11 10:04:37 -0400

    testing suite set up

--- src/inventory/inventory.service.spec.ts
+++ src/inventory/inventory.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { InventoryService } from './inventory.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('InventoryService', () => {
   let service: InventoryService;
+  let mockPrismaService: Partial<PrismaService>;
 
   beforeEach(async () => {
+    mockPrismaService = {
+      // Mock the methods used by StockService
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InventoryService],
+      providers: [
+        InventoryService,
+        { provide: PrismaService, useValue: mockPrismaService },
+      ],
     }).compile();
 
     service = module.get<InventoryService>(InventoryService);
